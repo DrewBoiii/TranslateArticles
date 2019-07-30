67de65c027f052d8a9b3aa4df14570c5b7516ff0
Drew <goodchickenboy@mail.ru>
2019-07-30 18:09:19 +0400

    init

---
+++ src/main/java/translatearticles/persistence/dao/ArticleRepository.java
@@ -0,0 +1,9 @@
+package translatearticles.persistence.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository {
+
+
+
+}
---
+++ src/main/java/translatearticles/persistence/model/Article.java
@@ -0,0 +1,4 @@
+package translatearticles.persistence.model;
+
+public class Article {
+}
